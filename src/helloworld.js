825870f71354061d87315b67d0854ca825d4244c
Christian Lilley <christian_lilley@mckinsey.com>
2015-05-11 20:03:55 +0800

    Add offline build;

--- src/helloworld.js
+++ src/helloworld.js
@@ -1,3 +1,5 @@
-React.render( < h1 > Hello, world! < /h1>,
+console.log('src');
+
+React.render( < h1 > Hello, worldz! < /h1>,
     document.getElementById('example')
 );
---
+++ to-dos.md
@@ -0,0 +1,5 @@
+#To-Do's:
+***************
+
+1. `npm install -g react-tools`
+2. `jsx --watch src/ build/`
